3385f33f9a855ef45b5b1680d5b7ede16960faca
Francesco Oliva <olibartfast@gmail.com>
2023-08-25 13:46:35 +0200

    Added spdlog lib for logging

--- inc/Detector.hpp
+++ inc/Detector.hpp
@@ -17,7 +17,6 @@ protected:
 	std::string backend_;
 	bool use_gpu_;
 public:
-
 	Detector(const std::string& modelBinary,
 	bool use_gpu = false, 
 	float confidenceThreshold = 0.5f, 
@@ -29,49 +28,7 @@ public:
 		network_width_ {network_width},
   		network_height_ {network_height}		 
 	{
-		backend_ = getInferenceBackend();
 	}
 
-
-	std::string getInferenceBackend()
-	{
-		const auto weights = modelBinary_;
-		std::string backend;	
-		size_t extension_pos = weights.rfind('.');
-		if (extension_pos != std::string::npos)
-		{
-			std::string extension = weights.substr(extension_pos + 1);
-			std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
-
-			if (extension == "onnx")
-			{
-				// Use ONNX Runtime as the backend
-				backend = "ONNX_RUNTIME";
-			}
-			else if (extension == "pt" || extension == "pth" || extension == "torchscript")
-			{
-				// Use LibTorch as the backend
-				backend = "LIBTORCH";
-			}
-			else if (extension == "trt" || extension == "engine" || extension == "plan")
-			{
-				// Use TensorRT as the backend
-				backend = "TENSORRT";
-			}
-			else
-			{
-				//logger->error("Invalid weight file extension. Supported extensions: .onnx, .pt, .pth, .trt");
-				std::cout << "Invalid weight file extension. Supported extensions: .onnx, .pt, .pth, .trt" << std::endl;
-				std::exit(1);
-			}
-		}
-		else
-		{
-			//logger->error("Invalid weight file path");
-			std::cout << "Invalid weight file path" << std::endl;
-			std::exit(1);
-		}
-		return backend;
-	}	
     virtual std::vector<Detection> run_detection(const cv::Mat& frame) = 0;
 };
--- inc/common.hpp
+++ inc/common.hpp
@@ -10,3 +10,7 @@
 #include <iomanip>
 #include <ctime>
 #include <filesystem>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/basic_file_sink.h>
