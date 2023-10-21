f0b0b7027c38b39b407c4f8ad8958dc9da21aafa
Francesco Oliva <olibartfast@gmail.com>
2023-10-21 13:54:47 +0200

    Started libtorch module refactoring

--- cmake/SetCompilerFlags.cmake
+++ cmake/SetCompilerFlags.cmake
@@ -1,8 +1,8 @@
 if (CMAKE_CUDA_COMPILER)
-    # If CUDA is available but not using TensorRT, set the CUDA flags
+    # If CUDA is available but not using TensorRT or LibTorch, set the CUDA flags
     set_target_properties(${PROJECT_NAME} PROPERTIES CUDA_SEPARABLE_COMPILATION ON)
 
-    # Set any specific CUDA flags for non-TensorRT code here
+    # Set any specific CUDA flags for non-TensorRT and non-LibTorch code here
     set(CUDA_ARCH_FLAG "--expt-extended-lambda") # CUDA compiler option that enables support for C++11 lambdas in device code.
 else()
     # If CUDA is not available, set CPU flags
@@ -10,8 +10,14 @@ else()
     set(CUDA_ARCH_FLAG "")
 endif()
 
-# Set common optimization flags
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -ffast-math")
+# Check if LibTorch is enabled
+if (USE_LIBTORCH)
+    # Add LibTorch-specific flags, including ${TORCH_CXX_FLAGS}
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
+else()
+    # If LibTorch is not enabled, set common optimization flags
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -ffast-math")
+endif()
 
 # Set debug flags
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g")
---
+++ src/libtorch/LibtorchInfer.hpp
@@ -0,0 +1,1 @@
+#include "LibtorchInfer.hpp"
