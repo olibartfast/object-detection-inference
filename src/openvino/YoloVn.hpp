33cf4fba2d919cbd7226013bb1e7297bfab40746
Francesco Oliva <olibartfast@gmail.com>
2024-01-01 18:02:10 +0100

    YOLOv5 OpenVino inference work in progress

--- src/openvino/YoloVn.hpp
+++ src/openvino/YoloVn.hpp
@@ -11,9 +11,8 @@ class YoloVn : public OVInfer
 
         cv::Rect get_rect(const cv::Size& imgSz, const std::vector<float>& bbox);
 
-        std::vector<float> preprocess_image(const cv::Mat& image) override{return   std::vector<float> {};}
-        std::vector<Detection> postprocess(const std::vector<std::vector<float>>& outputs, const std::vector<std::vector<int64_t>>& shapes, const cv::Size& frame_size) override
-        {
-            return std::vector<Detection>{};
-        }    
+        std::vector<float> preprocess_image(const cv::Mat& image) override;
+        std::vector<Detection> postprocess(const std::vector<std::vector<float>>& outputs, 
+            const std::vector<std::vector<int64_t>>& shapes, const cv::Size& frame_size) override;
+   
     };
