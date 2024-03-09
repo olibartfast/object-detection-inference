bb9bc318239aa672c388e8d2c4944c6b15d17d6c
Francesco Oliva <olibartfast@gmail.com>
2024-03-09 02:47:19 +0100

    Common postprocessing for yolov5 v6 v7 v8 v9

--- inc/DetectorSetup.hpp
+++ inc/DetectorSetup.hpp
@@ -3,8 +3,6 @@
 #include "YoloV4.hpp"
 #include "YoloVn.hpp"
 #include "YoloNas.hpp"
-#include "YoloV8.hpp"
-#include "YoloV9.hpp"
 #include "RtDetr.hpp"
 #include "RtDetrUltralytics.hpp"
 
@@ -13,21 +11,15 @@ std::unique_ptr<Detector> createDetector(
     const std::string& detectorType)
  {
     std::unique_ptr<Detector> detector{nullptr};
-    if(detectorType.find("yolov8") != std::string::npos)  
-    {
-        return std::make_unique<YoloV8>();
-    } 
-    else if(detectorType.find("yolov9") != std::string::npos)  
-    {
-        return std::make_unique<YoloV9>();
-    } 
-    else if(detectorType.find("yolov4") != std::string::npos)
+    if(detectorType.find("yolov4") != std::string::npos)
     {  
         return std::make_unique<YoloV4>();
     }   
     else if(detectorType.find("yolov5") != std::string::npos || 
         detectorType.find("yolov6") != std::string::npos  ||
-        detectorType.find("yolov7") != std::string::npos)  
+        detectorType.find("yolov7") != std::string::npos ||
+        detectorType.find("yolov8") != std::string::npos ||
+        detectorType.find("yolov9") != std::string::npos)  
     {
         return std::make_unique<YoloVn>();
     }
