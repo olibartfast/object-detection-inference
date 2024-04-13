46a39d6c11112eef7ad634a437971e56fc53d257
Francesco Oliva <olibartfast@gmail.com>
2024-04-13 23:05:01 +0200

    modified yolov4 postprocess

--- inc/DetectorSetup.hpp
+++ inc/DetectorSetup.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "common.hpp"
-// #include "YoloV4.hpp"
+#include "YoloV4.hpp"
 #include "YoloVn.hpp"
 #include "YoloNas.hpp"
 #include "RtDetr.hpp"
@@ -11,11 +11,11 @@ std::unique_ptr<Detector> createDetector(
     const std::string& detectorType)
  {
     std::unique_ptr<Detector> detector{nullptr};
-    // if(detectorType.find("yolov4") != std::string::npos)
-    // {  
-    //     return std::make_unique<YoloV4>();
-    // }  else
-    if(detectorType.find("yolov5") != std::string::npos || 
+    if(detectorType.find("yolov4") != std::string::npos)
+    {  
+        return std::make_unique<YoloV4>();
+    }  
+    else if(detectorType.find("yolov5") != std::string::npos || 
         detectorType.find("yolov6") != std::string::npos  ||
         detectorType.find("yolov7") != std::string::npos ||
         detectorType.find("yolov8") != std::string::npos ||
@@ -23,8 +23,7 @@ std::unique_ptr<Detector> createDetector(
     {
         return std::make_unique<YoloVn>();
     }
-    else 
-    if(detectorType.find("yolonas") != std::string::npos)  
+    else if(detectorType.find("yolonas") != std::string::npos)  
     {
         return std::make_unique<YoloNas>();
     } 
