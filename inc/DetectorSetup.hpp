38117c8ac26ca4d3b673efb98547da7a18a04429
Francesco Oliva <olibartfast@gmail.com>
2024-04-13 17:36:53 +0200

    Added std::any refactoring for code suppoerting yolov5...v9 series

--- inc/DetectorSetup.hpp
+++ inc/DetectorSetup.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "common.hpp"
 // #include "YoloV4.hpp"
-// #include "YoloVn.hpp"
+#include "YoloVn.hpp"
 #include "YoloNas.hpp"
 #include "RtDetr.hpp"
 #include "RtDetrUltralytics.hpp"
@@ -14,16 +14,16 @@ std::unique_ptr<Detector> createDetector(
     // if(detectorType.find("yolov4") != std::string::npos)
     // {  
     //     return std::make_unique<YoloV4>();
-    // }   
-    // else if(detectorType.find("yolov5") != std::string::npos || 
-    //     detectorType.find("yolov6") != std::string::npos  ||
-    //     detectorType.find("yolov7") != std::string::npos ||
-    //     detectorType.find("yolov8") != std::string::npos ||
-    //     detectorType.find("yolov9") != std::string::npos)  
-    // {
-    //     return std::make_unique<YoloVn>();
-    // }
-    // else 
+    // }  else
+    if(detectorType.find("yolov5") != std::string::npos || 
+        detectorType.find("yolov6") != std::string::npos  ||
+        detectorType.find("yolov7") != std::string::npos ||
+        detectorType.find("yolov8") != std::string::npos ||
+        detectorType.find("yolov9") != std::string::npos)  
+    {
+        return std::make_unique<YoloVn>();
+    }
+    else 
     if(detectorType.find("yolonas") != std::string::npos)  
     {
         return std::make_unique<YoloNas>();
--- src/detectors/YoloVn.hpp
+++ src/detectors/YoloVn.hpp
@@ -48,6 +48,6 @@ public:
     }
 
 
-    std::tuple<std::vector<cv::Rect>, std::vector<float>, std::vector<int>> postprocess_v567(const float* output, const std::vector<int64_t>& shape, const cv::Size& frame_size);
-    std::tuple<std::vector<cv::Rect>, std::vector<float>, std::vector<int>> postprocess_v89(const float* output, const std::vector<int64_t>& shape, const cv::Size& frame_size);
+    std::tuple<std::vector<cv::Rect>, std::vector<float>, std::vector<int>> postprocess_v567(const std::any* output, const std::vector<int64_t>& shape, const cv::Size& frame_size);
+    std::tuple<std::vector<cv::Rect>, std::vector<float>, std::vector<int>> postprocess_v89(const std::any* output, const std::vector<int64_t>& shape, const cv::Size& frame_size);
 };
