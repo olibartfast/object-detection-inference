8e5de4e8d53726128e2bf9037880c5c277d84f87
Francesco Oliva <olibartfast@gmail.com>
2024-07-04 01:59:11 +0200

    included and use the InferenceEngine  library from another GitHub project

--- cmake/OpenCVdnn.cmake
+++
@@ -1,8 +0,0 @@
-set(OPENCV_DNN_SOURCES
-${INFER_ROOT}/src/opencv-dnn/OCVDNNInfer.cpp
-# Add more OpenCV DNN source files here if needed
-)
-
-list(APPEND SOURCES ${OPENCV_DNN_SOURCES})
-
-add_compile_definitions(USE_OPENCV_DNN)
