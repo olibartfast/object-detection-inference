248ef974779e618cadb420653705f5b57792808b
Francesco Oliva <olibartfast@gmail.com>
2024-07-15 01:20:56 +0200

    refactored main structure to improve modularity

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -2,64 +2,12 @@
 #include "common.hpp"
 
 
-bool isDirectory(const std::string& path) {
-    std::filesystem::path fsPath(path);
-    return std::filesystem::is_directory(fsPath);
-}
+bool isDirectory(const std::string& path);
+bool isFile(const std::string& path);
 
-bool isFile(const std::string& path) {
-    return std::filesystem::exists(path);
-}
+void draw_label(cv::Mat& input_image, const std::string& label, float confidence, int left, int top);
 
 
-void draw_label(cv::Mat& input_image, const std::string& label, float confidence, int left, int top)
-{
-    const float FONT_SCALE = 0.7;
-    const int FONT_FACE = cv::FONT_HERSHEY_DUPLEX; // Change font type to what you think is better for you
-    const int THICKNESS = 2;
-    cv::Scalar YELLOW = cv::Scalar(0, 255, 255);
+std::vector<std::string> readLabelNames(const std::string& fileName);
 
-    // Display the label and confidence at the top of the bounding box.
-    std::ostringstream out;
-    out << std::fixed << std::setprecision(2) << static_cast<int>(confidence * 100) / 100.0;
-    std::string scoreText = out.str();
-    
-    int baseLine;
-    std::string display_text = label + ": " + scoreText;
-    cv::Size label_size = cv::getTextSize(display_text, FONT_FACE, FONT_SCALE, THICKNESS, &baseLine);
-    top = cv::max(top, label_size.height);
-
-    // Top left corner.
-    cv::Point tlc = cv::Point(left, top);
-    // Bottom right corner.
-    cv::Point brc = cv::Point(left + label_size.width, top + label_size.height + baseLine);
-
-    // Draw black rectangle.
-    cv::rectangle(input_image, tlc, brc, cv::Scalar(255, 0, 255), cv::FILLED);
-
-    // Put the label and confidence on the black rectangle.
-    cv::putText(input_image, display_text, cv::Point(left, top + label_size.height), FONT_FACE, FONT_SCALE, YELLOW, THICKNESS);
-}
-
-
-std::vector<std::string> readLabelNames(const std::string& fileName)
-{
-    if(!std::filesystem::exists(fileName)){
-        std::cerr << "Wrong path to labels " <<  fileName << std::endl;
-        exit(1);
-    } 
-    std::vector<std::string> classes;
-    std::ifstream ifs(fileName.c_str());
-    std::string line;
-    while (getline(ifs, line))
-    classes.push_back(line);
-    return classes;   
-}
-
-std::string getFileExtension(const std::string& filename) {
-    size_t dotPos = filename.find_last_of(".");
-    if (dotPos != std::string::npos) {
-        return filename.substr(dotPos + 1);
-    }
-    return ""; // Return empty string if no extension found
-}
+std::string getFileExtension(const std::string& filename);
---
+++ src/ObjectDetectionApp.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "VideoCaptureFactory.hpp"
+#include "DetectorSetup.hpp"
+#include "InferenceBackendSetup.hpp"
+#include "utils.hpp"
+#include "common.hpp"
+
+struct AppConfig {
+    std::string detectorType;
+    std::string source;
+    std::string labelsPath;
+    std::string config;
+    std::string weights;
+    bool use_gpu;
+    bool enable_warmup;
+    bool enable_benchmark;
+    int benchmark_iterations;
+    float confidenceThreshold;
+};
+
+class ObjectDetectionApp {
+public:
+    ObjectDetectionApp(const AppConfig& config);
+    void run();
+
+private:
+    void warmup_gpu(const cv::Mat& image);
+    void benchmark(const cv::Mat& image);
+    void setupLogging(const std::string& log_folder = "./logs");
+    void processImage(const std::string& source);
+    void processVideo(const std::string& source);
+
+    AppConfig config;
+    std::unique_ptr<InferenceInterface> engine;
+    std::unique_ptr<Detector> detector;
+    std::vector<std::string> classes;
+};
+
+AppConfig parseCommandLineArguments(int argc, char *argv[]);
