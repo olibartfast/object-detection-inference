f55081612de6cde85ac8969f015e2bf5f92dc598
Francesco Oliva <olibartfast@gmail.com>
2024-11-05 00:14:53 +0100

    Added unit test for DetectorSetup factory module

--- detectors/test/test_DetectorSetup.cpp
+++ detectors/test/test_DetectorSetup.cpp
@@ -4,64 +4,75 @@
 
 // Test case for YoloV4
 TEST(DetectorSetupTest, CreateYoloV4Detector) {
-    auto detector = createDetector("yolov4");
+    auto detector = DetectorSetup::createDetector("yolov4");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloV4*>(detector.get()) != nullptr);
 }
 
 // Test case for YoloVn (catching multiple versions)
 TEST(DetectorSetupTest, CreateYoloVnDetector) {
-    auto detector = createDetector("yolov5");
+    auto detector = DetectorSetup::createDetector("yolov5");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloVn*>(detector.get()) != nullptr);
 
-    detector = createDetector("yolov6");
+    detector = DetectorSetup::createDetector("yolov6");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloVn*>(detector.get()) != nullptr);
 
-    detector = createDetector("yolov7");
+    detector = DetectorSetup::createDetector("yolov7");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloVn*>(detector.get()) != nullptr);
 
-    detector = createDetector("yolov8");
+    detector = DetectorSetup::createDetector("yolov8");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloVn*>(detector.get()) != nullptr);
 
-    detector = createDetector("yolov9");
+    detector = DetectorSetup::createDetector("yolov9");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloVn*>(detector.get()) != nullptr);
 }
 
 // Test case for YoloNas
 TEST(DetectorSetupTest, CreateYoloNasDetector) {
-    auto detector = createDetector("yolonas");
+    auto detector = DetectorSetup::createDetector("yolonas");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YoloNas*>(detector.get()) != nullptr);
 }
 
 // Test case for YOLOv10
 TEST(DetectorSetupTest, CreateYOLOv10Detector) {
-    auto detector = createDetector("yolov10");
+    auto detector = DetectorSetup::createDetector("yolov10");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<YOLOv10*>(detector.get()) != nullptr);
 }
 
 // Test case for RtDetrUltralytics
 TEST(DetectorSetupTest, CreateRtDetrUltralyticsDetector) {
-    auto detector = createDetector("rtdetrul");
+    auto detector = DetectorSetup::createDetector("rtdetrul");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<RtDetrUltralytics*>(detector.get()) != nullptr);
 }
 
 // Test case for RtDetr
 TEST(DetectorSetupTest, CreateRtDetrDetector) {
-    auto detector = createDetector("rtdetr");
+    auto detector = DetectorSetup::createDetector("rtdetr");
     EXPECT_NE(detector, nullptr);
     EXPECT_TRUE(dynamic_cast<RtDetr*>(detector.get()) != nullptr);
 }
 
 // Test case for unknown type
 TEST(DetectorSetupTest, CreateUnknownDetector) {
-    auto detector = createDetector("unknown");
-    EXPECT_EQ(detector, nullptr);
+    // Expect an exception to be thrown when an unknown detector type is passed
+    EXPECT_THROW(
+        {
+            try {
+                auto detector = DetectorSetup::createDetector("unknown");
+            } catch (const std::invalid_argument& e) {
+                // Ensure the exception message is correct (optional)
+                EXPECT_STREQ(e.what(), "Unknown detector type");
+                throw;
+            }
+        },
+        std::invalid_argument
+    );
 }
