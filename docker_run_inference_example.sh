dcc0535d5f69e091199830c674e2ab27ca71a7ba
Francesco Oliva <olibartfast@gmail.com>
2025-01-26 12:55:21 +0100

    Added docker tensorrt inference example

---
+++ docker_run_inference_example.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+docker run --gpus=all --rm \
+    -v $(pwd)/data:/app/data \
+    -v /home/oli/repos/D-FINE/exports:/weights \
+    -v $(pwd)/labels:/labels \
+    object-detection-inference:tensorrt \
+    --type=dfine \
+    --weights=/weights/model.engine \
+    --source=/app/data/dog.jpg \
+    --labels=/labels/coco.names \
+    --input_sizes='3,640,640;2' 
+
+
+# docker run --rm \
+#     -v data:/app/data \
+#     -v /home/oli/repos/D-FINE:/weights \
+#     -v labels:/labels \
+#     -w /app/data \
+#     --entrypoint ls \
+#     object-detection-inference:tensorrt  \
+#     -alt /app/app
