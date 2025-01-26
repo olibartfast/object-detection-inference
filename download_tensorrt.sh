7988f484f6a9f17f232ea35c4fa2827dec2aa988
Francesco Oliva <olibartfast@gmail.com>
2025-01-26 22:52:17 +0100

    Update download_tensorrt.sh

--- download_tensorrt.sh
+++ download_tensorrt.sh
@@ -5,7 +5,7 @@ TRT_PATCH=.0
 TRT_BUILD=.23
 TRT_VERSION=${TRT_MAJOR}${TRT_MINOR}${TRT_PATCH}${TRT_BUILD}
 TRT_CUDA_VERSION=12.6
-#wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/${TRT_MAJOR}${TRT_MINOR}${TRT_PATCH}/tars/TensorRT-${TRT_VERSION}.Linux.x86_64-gnu.cuda-${TRT_CUDA_VERSION}.tar.gz 
+wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/${TRT_MAJOR}${TRT_MINOR}${TRT_PATCH}/tars/TensorRT-${TRT_VERSION}.Linux.x86_64-gnu.cuda-${TRT_CUDA_VERSION}.tar.gz 
 
 tar -xvf TensorRT-${TRT_VERSION}.Linux.x86_64-gnu.cuda-${TRT_CUDA_VERSION}.tar.gz
 
