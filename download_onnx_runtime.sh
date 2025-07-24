6e713dfd7d59d38ff21e69afdf73ff8e5d79e7e7
Francesco Oliva <olibartfast@gmail.com>
2025-07-24 23:20:25 +0200

    Remove deprecated download scripts and add versions.env for dependency management

--- download_onnx_runtime.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ORT_VERSION=1.19.2
-ONNX_RUNTIME_DIR=$HOME/onnxruntime-linux-x64-gpu-$ORT_VERSION 
-
-wget https://github.com/microsoft/onnxruntime/releases/download/v${ORT_VERSION}/onnxruntime-linux-x64-gpu-${ORT_VERSION}.tgz && \
-tar -xzvf onnxruntime-linux-x64-gpu-${ORT_VERSION}.tgz
-mv onnxruntime-linux-x64-gpu-${ORT_VERSION} $ONNX_RUNTIME_DIR
